bebd38a2b3e9a80730789237bd02bac0ef41e75e
Hao Tran <gambogo88@gmail.com>
2014-11-15 19:45:00 +0700

    Language Setting

---
+++ Otta/Lib/Language/NSString+Language.h
@@ -0,0 +1,15 @@
+//
+//  NSString+Common.h
+//  GroundTruth_Platfrom_SmartPhone_iOS
+//
+//  Created by Gambogo on 11/13/14.
+//  Copyright (c) 2014 TMA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Language)
+
+-(NSString*) toCurrentLanguage;
+
+@end
---
+++ Otta/Lib/Language/NSString+Language.m
@@ -0,0 +1,18 @@
+//
+//  NSString+Common.m
+//  GroundTruth_Platfrom_SmartPhone_iOS
+//
+//  Created by Gambogo on 11/13/14.
+//  Copyright (c) 2014 TMA. All rights reserved.
+//
+
+#import "NSString+Language.h"
+
+@implementation NSString (Language)
+
+-(NSString*) toCurrentLanguage
+{
+    return GetLocalizedString(self, nil);
+}
+
+@end
