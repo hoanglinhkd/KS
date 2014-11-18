8b45888839a23f3855ad11a4594b38aeaa130194
ThienChau <duythien0110@gmail.com>
2014-11-18 23:00:46 +0700

    fixing select menu bug, add logout button

--- Otta/ViewController/SideMenuViewController/SideMenuViewController.m
+++ Otta/ViewController/SideMenuViewController/SideMenuViewController.m
@@ -44,6 +44,9 @@
         [lastCellSelected.lblText setFont:[UIFont fontWithName:@"OpenSans-Light" size:18.00f]];
     }
     [_btnAbout.titleLabel setFont:[UIFont fontWithName:@"OpenSans-Bold" size:18.00f]];
+    
+    selectedSideIndex = -1;
+    [_menuTableView reloadData];
 }
 
 -(void) dehighlightAboutButton
@@ -73,7 +76,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 5;
+    return 6;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -126,6 +129,12 @@
             cell.lblText.text = [@"Settings" toCurrentLanguage];
         }
             break;
+        case 5:
+        {
+            cell.imgIcon.image = [UIImage imageNamed:@"menu_otta.png"];
+            cell.lblText.text = [@"About" toCurrentLanguage];
+        }
+            break;
             
         default:
             break;
@@ -135,26 +144,13 @@
     return cell;
 }
 
-/*- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
- {
- cell.backgroundColor = [UIColor clearColor];
- cell.textLabel.backgroundColor = [UIColor clearColor];
- cell.detailTextLabel.backgroundColor = [UIColor clearColor];
- }
- */
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
     [self dehighlightAboutButton];
-    OttaMenuCell *cell = (OttaMenuCell*)[tableView cellForRowAtIndexPath:indexPath];
-    lastCellSelected = cell;
-    [cell.lblText setFont:[UIFont fontWithName:@"OpenSans-Bold" size:18.00f]];
+    selectedSideIndex = indexPath.row;
+    [tableView reloadData];
+    
     [self performSegueWithIdentifier:@"segueAskQuestion" sender:nil];
-}
-
--(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    OttaMenuCell *cell = (OttaMenuCell*)[tableView cellForRowAtIndexPath:indexPath];
-    [cell.lblText setFont:[UIFont fontWithName:@"OpenSans-Light" size:18.00f]];
 }
 
 @end
