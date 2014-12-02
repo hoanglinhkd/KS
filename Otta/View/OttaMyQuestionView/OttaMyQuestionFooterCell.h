a90c272bbba83a9c62dc8a6ac1be27690eed06fe
Hao Tran <gambogo88@gmail.com>
2014-12-02 21:49:41 +0700

    add file for My Question feature

---
+++ Otta/View/OttaMyQuestionView/OttaMyQuestionFooterCell.h
@@ -0,0 +1,15 @@
+//
+//  OttaMyQuestionFooterCell.h
+//  Otta
+//
+//  Created by cscv on 12/2/14.
+//  Copyright (c) 2014 SojoDigital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OttaMyQuestionFooterCell : UIView
+@property (weak, nonatomic) IBOutlet UILabel *lblTime;
+
+- (IBAction)clickSeeAll:(id)sender;
+@end
---
+++ Otta/View/OttaMyQuestionView/OttaMyQuestionPictureCell.h
@@ -0,0 +1,13 @@
+//
+//  OttaMyQuestionPictureCell.h
+//  Otta
+//
+//  Created by cscv on 12/2/14.
+//  Copyright (c) 2014 SojoDigital. All rights reserved.
+//
+
+#import "OttaMyQuestionTextCell.h"
+
+@interface OttaMyQuestionPictureCell : OttaMyQuestionTextCell
+
+@end
