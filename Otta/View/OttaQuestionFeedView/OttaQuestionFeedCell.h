4a661bdaaf0da93563900c864ccb95b39b56c9c5
Hao Tran <gambogo88@gmail.com>
2014-12-17 08:11:42 +0700

    show demo data, add delegate action.

--- Otta/View/OttaQuestionFeedView/OttaQuestionFeedCell.h
+++ Otta/View/OttaQuestionFeedView/OttaQuestionFeedCell.h
@@ -17,6 +17,8 @@
 - (void)optionCell:(OttaQuestionFeedCell*)cell collapseBtnTapped:(id)row;
 - (void)optionCell:(OttaQuestionFeedCell*)cell imageBtnTappedAtRow:(id)row;
 
+- (void)optionCell:(OttaQuestionFeedCell*)cell didSelectRowAtIndexPath:(NSIndexPath*)idxPath; 
+
 @end
 
 @interface OttaQuestionFeedCell : UITableViewCell <UITableViewDataSource,  UITableViewDelegate>
